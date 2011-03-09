23f8df0d329c22ded4c13aa241276e108b7f02e3
Fabian Sörqvist <fabian.sorqvist@gmail.com>
2011-03-09 17:22:06 +0100

    more changes to puppet scripts

--- manifests/components/apache.pp
+++ manifests/components/apache.pp
@@ -1,18 +1,9 @@
 class apache {
-  package { httpd: ensure => installed }
+  package { apache2: ensure => installed }
 
   service {
-    httpd:
+    apache2:
     enable => true,
     ensure => running
   }
-
-  file { "/etc/httpd/conf/httpd.conf":
-      owner => root,
-      group => root,
-      mode => 660,
-      source => "/etc/puppet/files/etc/httpd/conf/httpd.conf",
-      require => [ Package[httpd] ]
-  }
-
 }
--- manifests/components/mysql.pp
+++ manifests/components/mysql.pp
@@ -1,21 +1,12 @@
 class mysql {
-
-  package { mysql: ensure => installed }
-  package { mysql-server: ensure => installed }
-
+  package { ["mysql-client", "mysql-common", "mysql-server"]:
+    ensure => installed
+  }
+  
   service {
-    mysqld:
+    mysql:
     enable => true,
     ensure => running,
     subscribe => Package[mysql-server]
   }
-
-  file { "/etc/my.cnf":
-      owner => root,
-      group => root,
-      mode => 660,
-      source => "/etc/puppet/files/etc/my.cnf",
-      require => [ Package[mysql-server] ]
-  }
-
 }
